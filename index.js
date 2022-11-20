52282f9627b5b904a467c52e4ae6c68f3ec38272
ngachadavid <davidngacha01@gmail.com>
2022-11-20 15:59:01 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(){
+    return number + number;
+}
+function substract() {
+    return number - number;
+}
+function multiply() {
+    return number * number;
+}
+function divide() {
+    return number / number;
+}
+var add = function(num1, num2) {
+    return num1+num2;
+}
+var subtract = function(num1, num2) {
+    return num1 - num2;
+}
+var multiply = function(num1, num2) {
+    return num1*num2;
+}
+var divide = function(num1, num2) {
+    return num1 / num2;
+}
+function increment(n) {
+    return n++;
+}
+function increment(n) {
+    n++
+    return n++
+}
+function decrement(n) {
+    n--
+    return n--
+}
+function makeInt(n) {
+    return parseInt(n);
+}
+function makeInt(n) {
+    return parseInt (n, "10")
+}
+function preserveDecimal(n) {
+    return (n);
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
